ef41155b2ab16193815df16525ba6fcbe82fd9bb
Jesper <jesper.hjertstedt@gmail.com>
2012-06-11 12:29:22 +0200

    capistrano changes
    Signed-off-by: Jesper <jesper.hjertstedt@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,13 @@ require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
 require 'erb'
 
-
 set :application, "super_upload"
 set :deploy_to, "/srv/apps/#{application}"
 
 set :scm, "git"
 set :repository,  "git@github.com:jesperhj/SuperUpload.git"
 set :branch, "master"
-#set :deploy_via, :remote_cache
+set :deploy_via, :remote_cache
 
 set :user, "ubuntu"
 set :ssh_options, { 
@@ -18,5 +17,22 @@ set :ssh_options, {
 }
 
 after 'deploy:update', 'deploy:cleanup'
-after 'deploy:setup', 'deploy:correct_permissions'
-after 'bundle:install', 'deploy:thin_files'
+after 'deploy:setup', 'deploy:correct_permissions', 'uploader:setup_server'
+after 'bundle:install', 'uploader:thin_files'
+
+
+namespace :uploader do
+  task :thin_files do
+    run "cp #{release_path}/thin/* #{shared_path}/bundle/ruby/1.8/gems/thin-1.3.1/lib/thin"
+  end
+  
+  task :setup_server do
+    top.upload('config/super_upload', '/tmp/super_upload', :via => :scp)
+    run "sudo cp /tmp/super_upload /etc/init.d/super_upload"
+    run "sudo chmod 755 /etc/init.d/super_upload"
+    run "sudo /usr/sbin/update-rc.d super_upload defaults"
+    
+    top.upload('config/super_upload.thin', '/tmp/super_upload.thin', :via => :scp)
+    run "sudo cp /tmp/super_upload.thin /etc/thin/super_upload.thin"
+  end
+end
--- config/deploy/aws.rb
+++ config/deploy/aws.rb
@@ -2,8 +2,6 @@
 # sunshine.test.videofy.me  : uses mock database
 role :app, "ec2-79-125-48-205.eu-west-1.compute.amazonaws.com"
 
-set :branch, "master"
-
 namespace :deploy do
   [:restart, :start, :stop].each do |t|
     desc "#{t} thin"
@@ -15,27 +13,5 @@ namespace :deploy do
   desc "Correct permissions"
   task :correct_permissions do
     sudo "chown -R ubuntu:ubuntu /srv/apps/#{application}"
-  end
-  
-  task :thin_files do
-    run "cp #{release_path}/thin/* #{shared_path}/bundle/ruby/1.8/gems/thin-1.3.1/lib/thin"
-  end
-  
-  task :setup_server do
-    upload(File.dirname(__FILE__)+'../super_upload', '/tmp/super_upload', :via => :scp)
-    run "sudo cp /tmp/super_upload /etc/init.d/super_upload"
-    run "sudo chmod 755 /etc/init.d/super_upload"
-    run "sudo /usr/sbin/update-rc.d super_upload defaults"
-    
-    upload(File.dirname(__FILE__)+'../super_upload.thin', '/tmp/super_upload.thin', :via => :scp)
-    run "sudo cp /tmp/super_upload.thin /etc/thin/super_upload.thin"
-    
-    upload(File.dirname(__FILE__)+'../nginx.conf', '/tmp/nginx.conf', :via => :scp)
-    run "sudo cp /tmp/nginx.conf /etc/nginx/nginx.conf"
-    
-    upload(File.dirname(__FILE__)+'../super_upload.nginx', '/tmp/super_upload.nginx')
-    run "sudo cp /tmp/super_upload.nginx /etc/nginx/super_upload.nginx"
-    run "sudo ln -s /etc/nginx/sites-available/super_upload.nginx /etc/nginx/sites-enabled/super_upload.nginx"
-    run "sudo service super_upload start"
   end
 end
